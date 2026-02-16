331d7b84556de4817f16e262aa2d878d520d381a
sismiksmansera <sismik.smansera@gmail.com>
2026-02-16 20:07:54 +0700

    feat: update JP table header to use two-row structure with 'Jam Pelajaran' and numbered columns 1-10

--- resources/views/admin/cek-presensi.blade.php
+++ resources/views/admin/cek-presensi.blade.php
@@ -756,12 +756,15 @@
                     <table class="jp-table">
                         <thead>
                             <tr>
-                                <th>No</th>
-                                <th>NISN</th>
-                                <th>Nama Siswa</th>
-                                <th>JP 1</th><th>JP 2</th><th>JP 3</th><th>JP 4</th><th>JP 5</th>
-                                <th>JP 6</th><th>JP 7</th><th>JP 8</th><th>JP 9</th><th>JP 10</th>
-                                <th>% Hadir</th>
+                                <th rowspan="2">No</th>
+                                <th rowspan="2">NISN</th>
+                                <th rowspan="2">Nama Siswa</th>
+                                <th colspan="10">Jam Pelajaran</th>
+                                <th rowspan="2">% Hadir</th>
+                            </tr>
+                            <tr>
+                                <th>1</th><th>2</th><th>3</th><th>4</th><th>5</th>
+                                <th>6</th><th>7</th><th>8</th><th>9</th><th>10</th>
                             </tr>
                         </thead>
                         <tbody id="jpTableBody">
